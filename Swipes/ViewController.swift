73e6bcfbc7e2f530775346b4a87354a6fc8c3aca
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 22:10:56 +0200

    SwipeGestures-iOS
    simple using swipe gestures

--- Swipes/ViewController.swift
+++ Swipes/ViewController.swift
@@ -7,13 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    var sounds = ["barr", "gun", "mp5", "rapid", "shot"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+           }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+            
+           var randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            
+            var filelocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+         
+            do {
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: filelocation!))
+                
+                player.play()
+            
+            } catch {}
+    
+        } 
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
